4ef2722aed0a780c0a23b05f11be4751e9db500a
ikechukwuakuma316 <ikechukwuakuma316@gmail.com>
2022-09-08 13:19:25 +0100

    first commit

---
+++ src/data/fixtures.ts
@@ -0,0 +1,126 @@
+export default {
+  author: "AI Uche",
+  hero: {
+    subtitle: "Front-end engineer",
+    description: "Building reliable and scalable websites, designs applications and ensures overall user accessibility.",
+    cta: "Browse Products",
+  },
+  products: {
+    featured: [
+      {
+        name: "Lorem",
+        // medium: "React · GraphQL · Golang",
+        summary: "Lorem ipsum lorem lorem",
+        copy: [
+          "Lorem ipsum lorem lorem"
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Discover",
+            // url: "https://doubletext.me/"
+          }
+        },
+        images: [
+          "/doubletext/0.png",
+          "/doubletext/0.png",
+          "/doubletext/0.png",
+          "/doubletext/0.png",
+          "/doubletext/0.png"
+        ]
+      },
+      {
+        name: "Lorem",
+        medium: "React · Next.js",
+        summary: "Lorem ipsum lorem lorem",
+        copy: [
+          "Lorem ipsum lorem lorem",
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Learn more",
+            // url: "https://climblist.com/"
+          }
+        },
+        images: [
+          "/climblist/0.png",
+          "/climblist/0.png",
+        ]
+      }
+    ],
+    // Only supports 0-3 at the moment
+    minor: [
+      {
+        name: "Name",
+        description: "Lorem ipsum lorem lorem",
+        link: {
+          name: "Github",
+          // url: "https://github.com/zendesk/volunteer_portal"
+        }
+      },
+      {
+        name: "Name",
+        description: "Lorem ipsum lorem lorem",
+        link: {
+          name: "Download",
+          // url: "https://samzx.itch.io/final-slash-vr"
+        }
+      },
+      {
+        name: "Name",
+        description: "Lorem ipsum lorem lorem",
+        link: {
+          name: "Demo",
+          // url: "https://mphwatch.samxie.net/demo"
+        }
+      }
+    ]
+  },
+  logoSection: {
+    title: "As hired by",
+    // Only supports 0-3 at the moment
+    logos: [
+      {
+        src: "/logos/unimelb.svg",
+        // url: "https://unimelb.edu.au/",
+        alt: "Lorem ipsum lorem lorem"
+      },
+      {
+        src: "/logos/zendesk.svg",
+        // url: "https://www.zendesk.com/",
+        alt: "Lorem ipsum lorem lorem"
+      },
+      {
+        src: "/logos/cashapp.svg",
+        // url: "https://cash.app/",
+        alt: "Lorem ipsum lorem lorem"
+      }
+  ]
+  },
+  footer: {
+    tagline: "AI Uche — Lagos, Nigeria",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/Tecyke1",
+        fa: "fab fa-twitter" // Code from: https://fontawesome.com/v5/icons/twitter?s=brands
+      },
+      {
+        name: "Github",
+        url: "https://github.com/ikechukwuakuma316",
+        fa: "fab fa-github"
+      },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/ikechukwu-akuma/",
+        fa: "fab fa-linkedin"
+      },
+      {
+        name: "Email",
+        url: "mailto:contact@ikechukwuakuma316.net",
+        fa: "fa fa-envelope"
+      },
+    ]
+  }
+}
