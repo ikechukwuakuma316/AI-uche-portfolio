8d4d29f64a66c1da516c5dd4c17ff6e7fe0c2b09
unknown <ikechukwuakuma316@gmail.com>
2022-09-23 21:19:12 +0100

    new changes

---
+++ README.md
@@ -0,0 +1,49 @@
+# aiuche Portfolio
+
+
+
+A neumorphic inspired personal website and portfolio.
+- Showcase your project screenshots with carousels
+- Link up your social and other links
+- Supports light and dark mode 💡
+
+
+## Deploy your own
+
+Clone and deploy with vercel.
+
+
+
+### Configurations
+- Under `src/data/fixtures.ts`, edit to customise copy.
+
+- Under `src/themes`, edit light and dark themes (including background image).
+
+- Render your own 3D model by editing `blender/peregrine-falcon.blend` using [Blender](https://www.blender.org/) for free.
+
+- Download company logos and use [Figma](https://www.figma.com/) to edit and export svg with no borders.
+
+Push your changes to master to auto-deploy to vercel.
+
+## Development
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn start
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/data/fixtures.ts
@@ -0,0 +1,131 @@
+export default {
+  author: "AI Uche",
+  hero: {
+    subtitle: "SOFTWARE DEVELOPER",
+    description: "Building reliable and scalable websites, designs applications and ensures overall user accessibility.",
+    cta: "Browse Products",
+  },
+  products: {
+    featured: [
+      {
+        name: "Lorem",
+        // medium: "React · GraphQL · Golang",
+        summary: "Lorem ipsum lorem lorem",
+        copy: [
+          "Lorem ipsum lorem lorem"
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Discover",
+            // url: "https://doubletext.me/"
+          }
+        },
+        images: [
+          "/doubletext/0.png",
+          "/doubletext/0.png",
+          "/doubletext/0.png",
+          "/doubletext/0.png",
+          "/doubletext/0.png"
+        ]
+      },
+      {
+        name: "Lorem",
+        medium: "React · Next.js",
+        summary: "Lorem ipsum lorem lorem",
+        copy: [
+          "Lorem ipsum lorem lorem",
+        ],
+        links: {
+          color: "#363636",
+          primary: {
+            name: "Learn more",
+            // url: "https://climblist.com/"
+          }
+        },
+        images: [
+          "/climblist/0.png",
+          "/climblist/0.png",
+        ]
+      }
+    ],
+    // Only supports 0-3 at the moment
+    minor: [
+      {
+        name: "Frontend",
+        description: "Front-end development is the process of building components that interact with users.",
+        link: {
+          name: "Github",
+          // url: "https://github.com/zendesk/volunteer_portal"
+        }
+      },
+      {
+        name: "Backend",
+        description: "Back-end development means working on server-side software, which focuses on everything you can't see on a website.",
+        link: {
+          name: "Download",
+          // url: "https://samzx.itch.io/final-slash-vr"
+        }
+      },
+      {
+        name: "UI/UX",
+        description: "UI and UX are two distinct facets of web design, yet they work hand-in-hand.",
+        link: {
+          name: "Demo",
+          // url: "https://mphwatch.samxie.net/demo"
+        }
+      }
+    ]
+  },
+  logoSection: {
+    title: "As hired by",
+    // Only supports 0-3 at the moment
+    logos: [
+      {
+        src: "/logos/unimelb.svg",
+        // url: "https://unimelb.edu.au/",
+        alt: "Lorem ipsum lorem lorem"
+      },
+      {
+        src: "/logos/zendesk.svg",
+        // url: "https://www.zendesk.com/",
+        alt: "Lorem ipsum lorem lorem"
+      },
+      {
+        src: "/logos/cashapp.svg",
+        // url: "https://cash.app/",
+        alt: "Lorem ipsum lorem lorem"
+      }
+  ]
+  },
+  footer: {
+    tagline: "AI Uche — Lagos, Nigeria",
+    links: [
+      {
+        name: "Twitter",
+        url: "https://twitter.com/Tecyke1",
+        fa: "fab fa-twitter" // Code from: https://fontawesome.com/v5/icons/twitter?s=brands
+      },
+      {
+        name: "Github",
+        url: "https://github.com/ikechukwuakuma316",
+        fa: "fab fa-github"
+      },
+      {
+        name: "Gitlab",
+        url: "https://gitlab.com/Savvy316",
+        fa: "fab fa-gitlab"
+      },
+      {
+        name: "Linkedin",
+        url: "https://www.linkedin.com/in/ikechukwu-akuma/",
+        fa: "fab fa-linkedin"
+      },
+      {
+        name: "Email",
+        url: "mailto:contact@ikechukwuakuma316.net",
+        fa: "fa fa-envelope"
+      },
+    ]
+  }
+}
